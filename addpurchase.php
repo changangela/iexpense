1c117a9d438d199f3193f6bd171db71736ac304a
changangela <angelachang27@gmail.com>
2016-11-19 16:33:45 -0500

    nfc works now

--- addpurchase.php
+++ addpurchase.php
@@ -13,10 +13,9 @@
 
 	$query = "INSERT INTO purchases (userid, itemlist, storeid, date) VALUES (" . $_SESSION['userid'].", '" . $itemlist."', ".$storeid.",'". $date ."')" ;
 	$result = mysqli_query($con, $query);
-	echo $query;
 	if($result){
 	}else{
 		echo "Connection unsuccessful, please try again later.";
 	}
-
+	Header("Location: home.php");
 ?>
--- uploadreceipt.php
+++ uploadreceipt.php
@@ -11,9 +11,11 @@
 	// check if form is submitted
 
 	if(isset($_GET['storeid'])){
-		$url = "addpurchase.php?storeid=" . $_GET['storeid'] . "&itemlist=" . $_GET['itemlist'] . "&date=" . date("Y-m-d",time());
-		Header("Location: " . $url);	
+		$url = "addpurchase.php?storeid=" . $_GET['storeid'] . "&itemlist=" . $_GET['itemlist'] . "&date=" . date("y-m-d",time());
+		echo $url;
+		Header("Location: " . $url);
 	}else{
-		header("home.php");
+		echo "Error. No request found.";
 	}
+
 ?>

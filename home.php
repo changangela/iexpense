233282e4761dbdf7652c02b967b2f031097d5c93
changangela <angelachang27@gmail.com>
2016-11-19 22:32:19 -0500

    fixed javascript files

--- home.php
+++ home.php
@@ -47,7 +47,7 @@
                 <div class = "collapse navbar-collapse" id = "navbar">
                     <ul class = "nav navbar-nav navbar-right">
 						<li>
-							<a href = "myreceipts.php">My receipts</a>
+							<a href = "myreceipts.php">My purchases</a>
 						</li>
 						<li>
 							<a href = "mywallet.php">My wallet</a>
@@ -197,23 +197,6 @@
 		<script src = "js/main.js"></script>
 
 		<script>
-	        $('.thumbnail').click(function(){
-		        // $('.modal-body').empty();
-		        var name = $(this).attr('name');
-		        var image = $(this).attr('image');
-		        var store = $(this).attr('store');
-		        var price = $(this).attr('price'); 
-		        var date = $(this).attr('date');
-		        var category = $(this).attr('category');
-
-		        $('.modal-title').text(name);
-		        $('.item-store').text(store);
-		        $('.item-price').text("$" + price);
-		        $('.modal-body .modal-img').attr('src',image);
-		        $('.item-date').text(date);
-		        $('.item-category').text(category);
-		        $('.item-location').text("York");
-		    });
 			$('.owl-carousel').owlCarousel({
 				loop: true,
 				items: 4
--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+    $('.thumbnail').click(function(){
+        // $('.modal-body').empty();
+        var name = $(this).attr('name');
+        var image = $(this).attr('image');
+        var store = $(this).attr('store');
+        var price = $(this).attr('price'); 
+        var date = $(this).attr('date');
+        var category = $(this).attr('category');
 
+        $('.modal-title').text(name);
+        $('.item-store').text(store);
+        $('.item-price').text("$" + price);
+        $('.modal-body .modal-img').attr('src',image);
+        $('.item-date').text(date);
+        $('.item-category').text(category);
+        $('.item-location').text("York");
+    });
 });

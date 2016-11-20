eeda56352cc15e4f58a41edcf9d8f09b23263e71
c27kwan <c27kwan@uwaterloo.ca>
2016-11-20 08:51:20 -0500

    fixed walletchart.js so that the display matches the data

--- walletchart.js
+++ walletchart.js
@@ -1,5 +1,12 @@
 window.onload = function () {
+    var personal = 17;
+    var technology= 20;
+    var groceries= 8;
+    var entertainment= 20;
+    var total = personal + technology + groceries +entertainment;
+
     var chart = new CanvasJS.Chart("chartmander", {
+        
         title: {
             text: "Expenses per category",
             fontFamily: "Verdana",
@@ -24,20 +31,19 @@ window.onload = function () {
         data: [
         {
             indexLabelFontSize: 26,
-            toolTipContent: "<span style='\"'color: {color};'\"'><strong>{indexLabel}</strong></span><span style='\"'font-size: 20px; color:peru '\"'><strong>{y}</strong></span>",
+            toolTipContent: "<span style='\"'color: {color};'\"'><strong>{indexLabel}</strong></span><span style='\"'font-size: 20px; color:peru '\"'><strong> {y}%</strong></span>",
 
             indexLabelPlacement: "inside",
-            indexLabelFontColor: "white",
+            indexLabelFontColor: "black",
             indexLabelFontWeight: 600,
             indexLabelFontFamily: "Verdana",
             color: "#62C9C3",
             type: "bar",
-            dataPoints: [
-                { y: 2, label: "21%", indexLabel: "Personal" },
-                { y: 25, label: "25%", indexLabel: "Groceries" },
-                { y: 33, label: "33%", indexLabel: "Entertainment" },
-                { y: 36, label: "36%", indexLabel: "Technology" },
-
+            dataPoints: [ 
+                { y: 100*entertainment/total, label: (100*entertainment/total).toFixed(2) + "%", indexLabel: "Entertainment" },
+                { y: 100*groceries/total, label: (100*groceries/total).toFixed(2) + "%", indexLabel: "Groceries" },
+                { y: 100*personal/total, label: (100*personal/total).toFixed(2) + "%", indexLabel: "Personal" },
+                { y: 100*technology/total, label: (100*technology/total).toFixed(2) + "%", indexLabel: "Technology" },                
             ]
         }
         ]

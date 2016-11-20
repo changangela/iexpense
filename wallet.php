a77be9730349291f69fff4914e1856e6a7bf51a0
changangela <angelachang27@gmail.com>
2016-11-20 04:57:44 -0500

    started wallet page

---
+++ wallet.php
@@ -0,0 +1,85 @@
+<?php
+	session_start();
+	if(!isset($_SESSION['userid'])){
+		header("Location: index.php");
+	}
+	include_once "connectdb.php";
+?>
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title> | iExpense</title>
+			<meta content = "width = device.width , initial-scale = 1.0" name = "viewport">
+			<link rel = "stylesheet" href = "vendor/font-awesome/css/font-awesome.min.css"/>
+	        <link rel = "stylesheet" href = "vendor/bootstrap/css/bootstrap.min.css" type="text/css"/>
+	        <link rel = "stylesheet" href = "vendor/owl-carousel/css/owl.carousel.css" type="text/css"/>
+	        <link rel = "stylesheet" href = "vendor/owl-carousel/css/owl.theme.css" type="text/css"/>
+	        <link rel = "stylesheet" href = "vendor/owl-carousel/css/owl.transitions.css" type="text/css"/>
+	        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet"/> 
+	        <link rel = "stylesheet" href = "css/main.css" type = "text/css"/>
+	</head>
+	<body>
+		<nav class = "navbar navbar-default" role = "navigation">
+            <div class = "container-fluid">
+                <div class= " navbar-header">
+                    <button type = "button" class = "navbar-toggle" data-toggle ="collapse" data-target = "#navbar">
+                        <span class ="sr-only"> Toggle navigation </span>
+                        <span class ="icon-bar"></span>
+                        <span class = "icon-bar"></span>
+                        <span class = "icon-bar"></span>
+                    </button>
+                    <a class = "navbar-brand" href= "index.php">
+                        iExpense
+                    </a>
+                </div>
+
+                <div class = "collapse navbar-collapse" id = "navbar">
+                    <ul class = "nav navbar-nav navbar-right">
+						<li>
+							<a href = "purchases.php">Purchases</a>
+						</li>
+						<li>
+							<a href = "employeeprofile.php">Employee profile</a>
+						</li>
+						<li>
+							<a href = "transactions.php">Transactions</a>
+						</li>
+						<li>
+							<a href = "wallet.php">Wallet</a>
+						</li>
+                        <li>
+                        	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        		<i class="icon-user" aria-hidden="true"></i>
+                         		 <?php echo $_SESSION['username'];?>
+                         		<span class="caret"></span>
+                         	</a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="logout.php">
+										<span class = "glyphicon glyphicon-log-out"></span>
+										 Logout
+									</a>
+								</li>
+							</ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+		<script src = "vendor/jquery/jquery-3.1.0.min.js"></script>
+		<script src = "vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src = "vendor/owl-carousel/js/owl.carousel.min.js"></script>
+		<script src = "js/main.js"></script>
+
+		<script>
+			$('.owl-carousel').owlCarousel({
+				loop: true,
+				items: 4
+			});
+		</script>
+	</body>
+</html>
